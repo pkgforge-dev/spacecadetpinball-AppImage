072198384d0b86af989e240b36907e2573ca3e47
Link4Electronics <link4electronics@gmail.com>
2026-08-18 20:25:39 -0300

    update template and hook (#11)

--- make-appimage.sh
+++ make-appimage.sh
@@ -5,7 +5,7 @@ set -eu
 ARCH=$(uname -m)
 export ARCH
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=https://raw.githubusercontent.com/k4zmu2a/SpaceCadetPinball/refs/heads/master/SpaceCadetPinball/Icon_128x128.png
 export DEPLOY_OPENGL=1
